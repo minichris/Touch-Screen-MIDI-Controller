156653d9d7df5794028f36e662ed76fa9197ab16
Zaperox <admin@carolineleeceramics.co.uk>
2017-08-12 17:09:42 +0100

    Commented MIDI controller stuff

--- TouchScreenMIDIController/TouchScreenMIDIController.ino
+++ TouchScreenMIDIController/TouchScreenMIDIController.ino
@@ -12,14 +12,14 @@ Adafruit_FT6206 ctp = Adafruit_FT6206();
 #define TFT_HEIGHT 315
 #define TFT_WIDTH 239
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-
+TS_Point p = TS_Point(0, 0, 0);
 #include <MIDI.h>
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 
 void setup(void) {
-  tft.begin();
-  tft.fillScreen(ILI9341_WHITE);
+  tft.begin(); //Initalize the display
+  tft.fillScreen(ILI9341_WHITE); //Set the entire display to be white
 
   if (! ctp.begin(40)) {  // pass in 'sensitivity' coefficient
     while (1);
@@ -31,28 +31,28 @@ void setup(void) {
 }
 
 void loop() {
-  // Wait for a touch
-  if (! ctp.touched()) {
-    return;
+  if (! ctp.touched()) { //if we wern't touched
+    return; //return to the start of the loop
   }
-
-  // Retrieve a point  
-  TS_Point p = ctp.getPoint();
-  int Modulation1 = (float)p.x / (float)TFT_WIDTH * (float)127;
-  int Modulation2 = (float)p.y / (float)TFT_HEIGHT * (float)127;
-  
-  // Print out raw data from screen touch controller
-  //tft.fillScreen(ILI9341_WHITE);
-  //tft.setCursor(150 , 150);
-  //tft.print(Modulation1);
-  //tft.setCursor(150 , 170);
-  //tft.print(Modulation2);
-  //pitch, velo, channel
-  //1 is the modulation wheel
-  //tft.drawFastHLine(0, p.y, TFT_WIDTH, ILI9341_BLACK);
-  //tft.drawFastVLine(p.x, 0, TFT_HEIGHT, ILI9341_BLACK);
-  
-  MIDI.send(midi::ControlChange, 1, (int)Modulation1, 0xC);
-  MIDI.send(midi::ControlChange, 1, (int)Modulation2, 0xD);
-
+  else{ //if we were touched
+    //tft.drawFastHLine(0, p.y, TFT_WIDTH, ILI9341_WHITE);
+    //tft.drawFastVLine(p.x, 0, TFT_HEIGHT, ILI9341_WHITE);
+    
+    p = ctp.getPoint(); //get the point of touching
+    int Modulation1 = (float)p.x / (float)TFT_WIDTH * (float)127; //get the amount of MIDI modulation (0 - 127) to apply based on the X of the touch
+    int Modulation2 = (float)p.y / (float)TFT_HEIGHT * (float)127; //get the amount of MIDI modulation (0 - 127) to apply based on the Y of the touch
+    MIDI.send(midi::ControlChange, 1, (int)Modulation1, 0xC); //send the modulation on the X over MIDI on 0xC
+    MIDI.send(midi::ControlChange, 1, (int)Modulation2, 0xD); //send the modulation on the Y over MIDI on 0xD
+    
+    // Print out raw data from screen touch controller
+    //tft.fillScreen(ILI9341_WHITE);
+    //tft.setCursor(150 , 150);
+    //tft.print(Modulation1);
+    //tft.setCursor(150 , 170);
+    //tft.print(Modulation2);
+    //pitch, velo, channel
+    //1 is the modulation wheel
+    tft.drawFastHLine(0, p.y, TFT_WIDTH, ILI9341_BLACK);
+    tft.drawFastVLine(p.x, 0, TFT_HEIGHT, ILI9341_BLACK);
+  }
 }
