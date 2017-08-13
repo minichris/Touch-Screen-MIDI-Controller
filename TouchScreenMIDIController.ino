55b8a838cbff9ba537f2f0d66a8ea71602a93484
Zaperox <admin@carolineleeceramics.co.uk>
2017-08-13 20:45:55 +0100

    Commenting, Code cleanup and more Adafruit libraries

--- TouchScreenMIDIController.ino
+++ TouchScreenMIDIController.ino
@@ -1,47 +1,52 @@
-#include <GFX.h>        // Core graphics library
-#include <SPI.h>        // this is needed for display
-#include <Wire.h>       // this is needed for FT6206
-#include <FT6206.h>
-#include <Adafruit_ILI9341.h>
+#include <Adafruit_GFX.h>       // Core graphics library
+#include <SPI.h>                // this is needed for display
+#include <Wire.h>               // this is needed for FT6206
+#include <FT6206.h>             // we need to use this one because the Adafuit one doesn't work with our screen. (This will be fixed by this https://github.com/adafruit/Adafruit_FT6206_Library/pull/5)
+#include <Adafruit_ILI9341.h>   // this is needed for the display
+#include <MIDI.h>               // MIDI stuff
 
-FT6206 ctp = FT6206();
 #define TFT_CS 9
 #define TFT_DC 7
 #define TFT_HEIGHT 315
 #define TFT_WIDTH 239
 #define BASE_COLOR ILI9341_WHITE
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
+FT6206 TouchScreen = FT6206();
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 TS_Point p = TS_Point(0, 0, 0);
 TS_Point p_old  = TS_Point(0, 0, 0);
-int Modulation1, Modulation2; 
 
-#include <MIDI.h>
+
 MIDI_CREATE_DEFAULT_INSTANCE();
-
 
 void setup(void) {
   tft.begin();
-  tft.fillScreen(BASE_COLOR);
-
-  if (! ctp.begin(40)) {  // pass in 'sensitivity' coefficient
-    while (1);
-  }
-
   tft.setTextSize(2);
   tft.setTextColor(ILI9341_BLACK);
+  tft.fillScreen(BASE_COLOR);
+  tft.setCursor(20 , TFT_HEIGHT / 2); //set the location of the text output
+  tft.print("Please wait"); //Tell the user we are calibrating
+  tft.setCursor(20 , (TFT_HEIGHT / 2) + 20); //set the location of the text output
+  tft.print("for calibration..."); //Tell the user we are calibrating
+  
+  if (!TouchScreen.begin(30)) {  // pass in 'sensitivity' coefficient
+    while (1);
+  }
+  TouchScreen.autoCalibrate(); //calibrate down
+  
   MIDI.begin(4);
+  tft.fillScreen(ILI9341_WHITE);
 }
 
 void loop() {
   // Wait for a touch
-  if (! ctp.touched()) {
-    return;
+  if (! TouchScreen.touched()) {
+    return; //if we arn't touched this loop, skip to the next loop
   }
   else {
-    p = ctp.getPoint(); //get the point of the touch
-    Modulation1 = ((float)p.x / (float)TFT_WIDTH * (float)127) - 1; //work out the new modulations
-    Modulation2 = ((float)p.y / (float)TFT_HEIGHT * (float)127) - 1;//work out the new modulations
+    p = TouchScreen.getPoint(); //get the point of the touch
+    int Modulation1 = ((float)p.x / (float)TFT_WIDTH * (float)127); //work out the new modulations
+    int Modulation2 = ((float)p.y / (float)TFT_HEIGHT * (float)127);//work out the new modulations
     
     if(p != p_old){ //if the point of touch has changed
       //pitch, velo, channel
@@ -56,12 +61,12 @@ void loop() {
       tft.drawFastVLine(p.x, 0, TFT_HEIGHT, ILI9341_BLACK); //draw a new line
 
       // Print out raw data from screen touch controller
-      tft.fillRect(150, 150,  36, 36, BASE_COLOR);
-      tft.setCursor(150 , 150);
-      tft.print(Modulation1);
-      tft.setCursor(150 , 170);
-      tft.print(Modulation2);
+      tft.fillRect(150, 150,  36, 36, BASE_COLOR); //remove the previous modulation numbers
+      tft.setCursor(150 , 150); //set the location of the text output of mod1
+      tft.print(Modulation1); //print it
+      tft.setCursor(150 , 170); //set the location of the text output of mod2
+      tft.print(Modulation2); //print it
     }
-    p_old = p;
+    p_old = p; //set the old point to the current point
   }
 }
